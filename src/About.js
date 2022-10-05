31ff1c7f0fd29a65fc0cad1ade91aca3641d9835
Lucifer-iron <amithubrajpal@csaist.ac.in>
2022-10-05 21:10:11 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+
+
+const About=()=> {
+    
+        return (
+            <>
+            <div className='d-flex justify-content-center my-5 '>
+        <div class="card" style={{ width: "20rem" }}>
+        <img src="https://images.unsplash.com/photo-1572949645841-094f3a9c4c94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" class="card-img-top" alt="..."/>
+        <div className="card-body ">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="/" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      </div>
+      </>
+        )
+    
+}
+
+export default About
